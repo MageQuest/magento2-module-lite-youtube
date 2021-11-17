7545d533aed7ce41a8f1cbed6c255a130861b67b
John Hughes <johnh@Johns-MacBook-Pro-3.local>
2021-11-17 18:43:11 +0000

    Initial commit - module skeleton

---
+++ registration.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+/**
+ * MageQuest - https://magequest.io
+ * Copyright © MageQuest. All rights reserved.
+ * See LICENSE.md file for details.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'MageQuest_LiteYouTube',
+    __DIR__
+);
